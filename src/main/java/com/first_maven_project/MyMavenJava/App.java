f2545e335cf14b8a30010acebfd749e13490b415
SaiAbhinav36 <abhinavsai36@gmail.com>
2024-11-23 15:10:18 +0530

    first commit

---
+++ src/main/java/com/first_maven_project/MyMavenJava/App.java
@@ -0,0 +1,13 @@
+package com.first_maven_project.MyMavenJava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
